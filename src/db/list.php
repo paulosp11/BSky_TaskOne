c15529eefa7c209631bf67e0be5dde2090ab2506
paulosp11 <paulo.informat@gmail.com>
2021-04-04 21:56:02 +0800

    PSR standards

--- src/db/list.php
+++ src/db/list.php
@@ -1,12 +1,13 @@
 <?php
 
 function listDb() {  
- 
+
      require 'connection.php';
      $queryBuilder = $dbh->createQueryBuilder();
     
      $statement = $queryBuilder 
-     ->select('id as ID, name as Name, email as Email, phone as Phone, age as Age')
+     ->select('id as ID, name as Name, email as Email, 
+                         phone as Phone, age as Age')
      ->from('persons')
      ->groupBy('id, name')
     
